ba6c24042ee71c58bd3eeb3448422414fca677f4
sortiz <sergio.ortiz@gmail.com>
2018-05-11 22:18:21 +0200

    Fix config.py

--- config.py
+++ config.py
@@ -24,13 +24,13 @@ CELERYD_CONCURRENCY   = 4
 
 
 SECRET_KEY = 'development key' # change by your own
-DEBUG      = True
+DEBUG      = False
 
-USER_LOGIN_ENABLED          = True
+USER_LOGIN_ENABLED          = False
 ENABLE_NEW_LOGINS           = True
-ADMINS                      = ['sergio.ortiz@gmail.com']
+ADMINS                      = []
 BANNED_USERS                = []
 OAUTHLIB_INSECURE_TRANSPORT = True # True also behind firewall,  False -> require HTTPS
-GOOGLE_OAUTH_CLIENT_ID      = '629052210385-6vvkbbkn744lst4sg0s1l765424r3a66.apps.googleusercontent.com'
-GOOGLE_OAUTH_CLIENT_SECRET  = 'dztt2KCtqlTfowlTULSzdfpE'
+GOOGLE_OAUTH_CLIENT_ID      = ''
+GOOGLE_OAUTH_CLIENT_SECRET  = ''
 
