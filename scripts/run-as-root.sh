0e80f8c00b083ddfea997a3548b0749e959e5047
sortiz <sergio@prompsit.com>
2020-06-18 12:06:30 +0000

    libbboost removed from run-as-root.sh

--- scripts/run-as-root.sh
+++ scripts/run-as-root.sh
@@ -5,4 +5,4 @@ apt-get install -y python3-venv python3-dev libbz2-dev git \
                    libtool libpcre3-dev flex xsltproc gawk \
                    zip unzip libxml2-dev libxml2-utils pkg-config autoconf \
                    coreutils zlib1g-dev libbz2-dev automake git-core build-essential \
-                   libboost-all-dev software-properties-common default-jdk curl redis                  
+                   software-properties-common default-jdk curl redis                  
