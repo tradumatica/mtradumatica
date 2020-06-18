e5a9756e77dbfd7422d17c54fb508998dc901441
sortiz <sergio@prompsit.com>
2020-06-18 14:03:27 +0000

    re-adding libboost-all-dev

--- scripts/run-as-root.sh
+++ scripts/run-as-root.sh
@@ -5,4 +5,5 @@ apt-get install -y python3-venv python3-dev libbz2-dev git \
                    libtool libpcre3-dev flex xsltproc gawk \
                    zip unzip libxml2-dev libxml2-utils pkg-config autoconf \
                    coreutils zlib1g-dev libbz2-dev automake git-core build-essential \
-                   software-properties-common default-jdk curl redis                  
+                   software-properties-common default-jdk curl redis \
+                   libboost-all-dev                 
